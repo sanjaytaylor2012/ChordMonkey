cac534ee7ae7de72073edd31bc3d0545a0e367fa
sanjaytaylor2012 <66386109+sanjaytaylor2012@users.noreply.github.com>
2026-03-24 15:42:08 -0400

    udl fix

--- frontend/lib/utils.ts
+++ frontend/lib/utils.ts
@@ -1,9 +1,9 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
 
-export const url = "http://localhost:8000"
-// export const url = "https://5ywb7vjgv5.execute-api.us-east-1.amazonaws.com"
+// export const url = "http://localhost:8000"
+export const url = "https://5ywb7vjgv5.execute-api.us-east-1.amazonaws.com";
